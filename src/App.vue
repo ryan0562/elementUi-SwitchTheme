745a9405a5cbeaa78d9b57ef4cf2c17f01ca4b94
王伟1 <wangwei1@mti-sh.cn>
2022-08-17 13:00:49 +0800

    项目换肤，包括elementUi 一起变更

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <nav>
+      <el-button type="primary" @click="switchTheme">切换</el-button>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      theme:'light'
+    }
+  },
+  created() {
+    this.switchTheme()
+  },
+  methods: {
+    switchTheme() {
+      this.theme = this.theme === "light" ? "dark" : "light"
+      document.documentElement.setAttribute("data-theme", this.theme);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: var(--color-primary);
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
